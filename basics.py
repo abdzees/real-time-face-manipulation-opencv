7f44198b73cea0d9aa6ba6742c1583e3d0d63645
Abdullah Zeeshan <109596800+abdzees@users.noreply.github.com>
2025-08-02 00:29:16 +0200

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,31 +1,5 @@
-# # -*- coding: utf-8 -*-
-# """
-# Created on Mon May  3 19:18:29 2021
-
-# @author: droes
-# """
-# from numba import njit # conda install numba
-
-# @njit
-# def histogram_figure_numba(np_img):
-#     '''
-#     Jit compiled function to increase performance.
-#     Use some loops insteads of purely numpy functions.
-#     If you face some compile errors using @njit, see: https://numba.pydata.org/numba-doc/dev/reference/numpysupported.html
-#     In case you dont need performance boosts, remove the njit flag above the function
-#     Do not use cv2 functions together with @njit
-#     '''
-    
-#     # return r_bars, g_bars, b_bars
-#     return [0], [0], [0]
-
-
-
-# ####
 
 # ### All other basic functions
-
-# ####
 
 import cv2
 import numpy as np
@@ -94,4 +68,5 @@ def calculate_rgb_histogram(frame, bins=256):
         hists.append(hist.flatten())
     
     # OpenCV uses BGR, so return B, G, R to match RGB order expected by plotting
+
     return hists[2], hists[1], hists[0]  # R, G, B
